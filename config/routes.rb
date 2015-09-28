6ccdece4700f95a6cb52968b76bc05648430f1d2
Nithya Rao <bumblebeenr@gmail.com>
2015-09-28 18:10:10 -0500

    Inprogress with navigation links before adding member link page

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:update]
   resources :invitations
-  resources :items
-  get 'welcome/index'
+   get 'welcome/index'
   get 'signup/:invitation_token', to: 'devise/registrations#new', as: 'signup'
 
-  resources :groups, only: [:show,:new,:create,:edit, :index]
+  resources :groups, except: [:delete] do
+    resources :items
+  end 
   root to: 'welcome#index'
 
   end
