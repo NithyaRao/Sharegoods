b9e1dbb777863dea3cf04ae97f02148a2c2ccbfc
Nithya Rao <bumblebeenr@gmail.com>
2015-10-28 20:38:54 -0500

    Inprogress Back button navigation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,36 @@
 //= require turbolinks
 //= require_tree .
 
+if (history && history.pushState) {
+      $(function() {
+$('a[data-toggle="tab"]').on("click", function(e) {
+$.getScript(this.href);
+history.pushState(null, document.title, this.href);
+if ( document.activeElement.dataset.target == "#" ){
+document.activeElement.parentElement.click();
+}
+console.log ('^^^^'+ this.href + "&&&" + document.activeElement.dataset.target);
+e.preventDefault();
+});
+
+$('#header-nav').on("click", function(e) {
+$.getScript(this.href);
+history.pushState(null, document.title, this.href);
+console.log ('#####'+ location.href);
+});
+
+$(window).bind("popstate", function() {
+var activeTab = $('[href="' + location.pathname + '"]');
+if (activeTab.length) {
+activeTab.tab('show');
+} 
+else {
+$('.nav-tabs a:first').tab('show')
+}
+$.getScript(location.href);
+console.log ('****'+ location.href);
+});
+});
+
+}
 
